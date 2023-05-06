27ba28afdf50d3458b77a9ca7f2bbfc69985baf4
PhoneKhaingHein <pkh2662003@gmail.com>
2023-05-06 19:51:55 +0630

    Initialize project with component

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Book from './Book';
+import BookForm from './BookForm';
+
+const BookList = () => (
+  <div>
+    <Book />
+    <hr />
+    <BookForm />
+  </div>
+);
+
+export default BookList;
