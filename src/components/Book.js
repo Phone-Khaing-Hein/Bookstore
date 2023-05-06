5b55fe997f362313fbc094159ad3854cc20c8025
PhoneKhaingHein <pkh2662003@gmail.com>
2023-05-06 20:28:09 +0630

    initialize project with components

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Book = () => (
+const Book = ({ title, category, author }) => (
   <div className="card p-3 shadow">
-    <p className="text-black-50">Category</p>
-    <h2>Book Title</h2>
-    <p className="text-primary">Author</p>
+    <p className="text-black-50">{category}</p>
+    <h2>{title}</h2>
+    <p className="text-primary">{author}</p>
     <div>
       <button type="button" className="text-primary btn">Comment</button>
       <button type="button" className="text-primary btn">Edit</button>
@@ -12,5 +12,11 @@ const Book = () => (
     </div>
   </div>
 );
+
+Book.propTypes = {
+  title: String.isRequired,
+  category: String.isRequired,
+  author: String.isRequired,
+};
 
 export default Book;
