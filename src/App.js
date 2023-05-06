3779a19d443a16b00e2467ff4ac4b6c3974664e0
PhoneKhaingHein <pkh2662003@gmail.com>
2023-05-06 19:57:57 +0630

    project setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import BookList from './components/BookList';
-import Categories from './components/Categories';
-import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <main className="container">
-        <Routes>
-          <Route path="/" element={<BookList />} />
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
+        routes
       </main>
     </BrowserRouter>
   );
